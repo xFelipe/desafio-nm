e9778b9aaa09e749ce34a233023e1ea649ea0c91
Felipe Gomes Duarte <xfelipegd@gmail.com>
2021-10-13 00:45:29 -0300

    Validate if file was sent.

--- api/app.py
+++ api/app.py
@@ -5,6 +5,7 @@ from tasks import resize_image
 
 IMAGES_FOLDER = os.environ.get('IMAGES_PATH') or '../images'
 RECEIVED_IMAGES_FOLDER = os.path.join(IMAGES_FOLDER, 'received_images')
+DATETIME_FORMAT = '%y_%m_%d__%H_%M_%S-'
 
 
 app = Flask(__name__)
@@ -13,16 +14,18 @@ app = Flask(__name__)
 def send_image():
     os.makedirs(RECEIVED_IMAGES_FOLDER, exist_ok=True)
 
-    file = request.files['image']
-    new_file_name = datetime.now().strftime('%y_%m_%d__%H_%M_%S-') + file.filename
+    file = request.files.get('image')
+    if not file:
+        return {'message': 'image field is required.'}, 400
+
+    new_file_name = datetime.now().strftime(DATETIME_FORMAT) + file.filename
     new_file_path = os.path.join(RECEIVED_IMAGES_FOLDER, new_file_name)
-    print("new_file_path:", new_file_path)
     with open(new_file_path, 'wb') as new_file:
         new_file.write(file.read())
 
     resize_image.delay(new_file_name, 384, 384)
 
-    return {'message': 'Task sent'}, 200
+    return {'message': 'Task sent', 'file_name': new_file_name}, 200
 
 
 if __name__ == '__main__':
